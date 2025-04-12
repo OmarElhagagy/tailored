000d097de6defc00f20483fd5f97db3ebc663dba
OmarElhagagy <oadeell21@gmail.com>
2025-04-12 20:05:39 +0200

    started auth.js route

--- sewing-platform-backend/routes/auth.js
+++ sewing-platform-backend/routes/auth.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const { check, validationResult } = require('express-validator');
+const user = require('../models/User');
+const auth = require('../middleware/auth');
+const sendEmai = require('../utils/sendEmail'); // will make the folder utils 
 
-router.get('/', (req, res) => {
-  res.send('Auth route placeholder');
-});
 
-module.exports = router;
+// @route /api/auth/register
+// @desc register user
+// access Public
+router.post('/register', [
+  check('role', 'Role is required').not().isEmpty(),
+  check('Password', 'Password must at least be 8 characters').isLength({ min: 8 }),
+  check('email', 'Please include a valid email').optional().isEmail(),
+  check('phone', 'Phone number is required')
+], async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password, role, phone, businessName, businessAddress } = req.body;
+  }
+)
