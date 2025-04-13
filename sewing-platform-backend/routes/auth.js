7810c37dddacb6a3650643aa50a663b86da0779f
OmarElhagagy <oadeell21@gmail.com>
2025-04-13 23:55:25 +0200

    fixed typo in auth.js

--- sewing-platform-backend/routes/auth.js
+++ sewing-platform-backend/routes/auth.js
@@ -6,10 +6,10 @@ const crypto = require('crypto');
 const { check, validationResult } = require('express-validator');
 const user = require('../models/User');
 const auth = require('../middleware/auth');
-const sendEmai = require('../utils/sendEmail'); // will make the folder utils 
+const sendEmail = require('../utils/sendEmail'); // will make the folder utils 
 
 
-// @route /api/auth/register
+// @route POST /api/auth/register
 // @desc register user
 // access Public
 router.post('/register', [
@@ -24,5 +24,57 @@ router.post('/register', [
     }
 
     const { name, email, password, role, phone, businessName, businessAddress } = req.body;
-  }
-)
+
+    try {
+      // check if user exists
+      if (email) {
+        const existingUser = await user.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ errors: [{ message: 'User already exists'}] });
+        }
+      }
+
+      //validate role specific details
+      if (role === 'buyer' && !name) {
+        return res.status(400).json({ errors: [{ message: 'Name field required'}] });
+      }
+
+      if (role === 'seller' && !businessName) {
+        return res.status(400).json({errors: [{ message: 'Business name field required'}]});
+      }
+
+      const user = new user ({
+        name,
+        email,
+        role,
+        businessName,
+        businessAddress,
+        phone,
+        password
+      });
+
+      await user.save();
+
+      // create and return JWT
+      const payload = {
+        user: {
+          id: user.id,
+          role: user.role
+        }
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {expiresIn : '24h'},
+        (err, token) => {
+          if (err) throw err;
+          res.json({token});
+        }
+      );
+    }catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  });
+
