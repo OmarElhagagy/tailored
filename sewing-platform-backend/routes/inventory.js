348c70bb380790716c672c89297fb9662c145a98
OmarElhagagy <oadeell21@gmail.com>
2025-04-16 00:01:58 +0200

    added  route   GET /api/inventory/:id

--- sewing-platform-backend/routes/inventory.js
+++ sewing-platform-backend/routes/inventory.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
+const checkRole = require('../middleware/checkRole');
+const InventoryItem = require('../models/InventoryItem');
+const uploadMiddleware = require('../middleware/upload');
 
-router.get('/', (req, res) => {
-  res.send('Auth route placeholder');
-});
+// @route GET /api/inventory
+// @desc  Get sellers inventory
+// @access Private (Seller only)
+router.get('/', auth, checkRole('seller'), async(req, res) => {
+  try {
+    const inventoryItems = await InventoryItem.find({ sellerId: req.user.id }).sort({ createdAt: -1 });
 
-module.exports = router;
+    res.json(inventoryItems);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+})
+
+// @route GET /api/inventory/:id
+// @desc  Get inventory item by id
+// @access Private (Owner only)
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const inventoryItem = await InventoryItem.find(req.params.id);
+
+    if (!inventoryItem) {
+      return res.status(404).json({ errors: [{ message: 'Inventory item not found' }] });
+    }
+
+    // check if user owns the inventory item
+    if (inventoryItem.sellerId.toString() !== req.user.id && req.user.role !== "admin") {
+      return res.status(403).json({ errors: [{ message: 'Not authorized' }] });
+    }
+
+    res.json(inventoryItem);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'objectId') {
+      return res.status(404).json({ errors: [{ message: 'Inventory item not found' }] });
+    }
+    res.status(500).send('Server error');
+  }
+})
