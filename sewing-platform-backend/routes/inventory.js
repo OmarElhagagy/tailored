cc392bfe376656059e1d842e284a209e47cd90cf
OmarElhagagy <oadeell21@gmail.com>
2025-04-16 02:46:25 +0200

    finished route POST /api/inventory

--- sewing-platform-backend/routes/inventory.js
+++ sewing-platform-backend/routes/inventory.js
@@ -48,5 +48,50 @@ router.get('/:id', auth, async (req, res) => {
 
 // @route POST /api/inventory
 // @desc  Create new inventory item
-// @access private
+// @access private (Seller only)
+router.post('/', [
+  auth,
+  checkRole('seller'),
+  uploadMiddleware.single('photo'),
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('stock', 'Stock quantity is required').isNumeric(),
+    check('threshold', 'threshold must be a number').optional().isNumeric()
+  ]
+], async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
 
+    try {
+      const { name, stock, threshold, description, tags, unit, sku, cost } = req.body
+
+      // handle tags (convert comma seperated strinf to array and trim it) so we can store it in mongodb
+      const parsedTags = tags ? tags.split(',').map(tag => tag.trim()) : [];
+
+      const newItem = new InventoryItem ({
+        name,
+        description,
+        stock: Number(stock),
+        threshold: threshold? Number(threshold) : 5,
+        sellerId: req.user.id,
+        unit,
+        sku,
+        tags: parsedTags,
+        cost: cost ? Number(cost) : undefined,
+      })
+
+      // Add photo path if uploaded
+      if (req.file) {
+        newItem.photo = req.file.path;
+      }
+
+      const inventoryItem = await newItem.save();
+      res.json(inventoryItem);
+    } catch(error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+)
